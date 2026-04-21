babe85b466d128ad7d71121606749137b7d7fe5f
constCrippa <constacrippa@gmail.com>
2026-04-21 13:58:06 -0300

    Update server.js

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
+app.use(express.static(__dirname));
 
 app.post('/api/chat', async (req, res) => {
   try {
